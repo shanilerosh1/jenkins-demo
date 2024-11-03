28149fe233d95c14a47f3380284cc23293c6cd4c
Shanil Erosh <shanil@planyear.com>
2024-11-03 23:22:51 +0530

    updated tc's

--- src/test/java/example/CalculatorTests.java
+++ src/test/java/example/CalculatorTests.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class CalculatorTests {
 
     @Test
-    @DisplayName("1 + 1 = 2")
+    @DisplayName("1 + 1 =  2")
     void addsTwoNumbers() {
         Calculator calculator = new Calculator();
         assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
