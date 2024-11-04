a1775e989311a83e8df32409a67d00c871c315bd
Shanil Erosh <shanil@planyear.com>
2024-11-04 12:48:37 +0530

    Added jenkins job

---
+++ MavenProjectDSL.groovy
@@ -0,0 +1,16 @@
+job("First-Maven-project-via-dsm"){
+    description("This is a Maven project")
+    scm {
+        git("https://github.com/shanilerosh1/jenkins-demo.git", 'master')
+    }
+    triggers {
+        scm("* * * * *")
+    }
+    steps {
+        maven('clean package', 'C:\\Users\\Shanil\\.jenkins\\workspace\\jsp-build\\java-tomcat-sample\\pom.xml')
+    }
+    publishers {
+        archiveArtifacts '**/*.war'
+    }
+
+}
